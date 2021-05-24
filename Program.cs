30b77d80903036b5d2d7910cf2429fe68f75fad9
ucrengineerpy@gmail.com <ucrengineerpy@gmail.com>
2021-05-23 18:47:11 -0700

    testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TradingApplication___Console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test.");
         }
     }
 }
